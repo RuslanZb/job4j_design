7b60e5686078c99a4d9914de67fd1973a7acf8d3
RuslanZb <zubairov.rr@mail.ru>
2022-06-07 11:53:09 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,30 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Analize {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int add = 0;
+        int ch = 0;
+        int del = previous.size();
+        Map<Integer, String> previousMap = new HashMap<>();
+        for (User prev : previous) {
+            previousMap.put(prev.getId(), prev.getName());
+        }
+        for (User curr : current) {
+            if (!previousMap.containsKey(curr.getId()) && !previousMap.containsValue(curr.getName())) {
+                add++;
+            }
+            if (previousMap.containsKey(curr.getId()) && !previousMap.containsValue(curr.getName())) {
+                ch++;
+            }
+            if (previousMap.containsKey(curr.getId())) {
+                del--;
+            }
+        }
+        return new Info(add,ch,del);
+    }
+}
