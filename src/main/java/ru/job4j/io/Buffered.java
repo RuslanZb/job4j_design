371d7a3f5fb5dbe848e2f202af65b757aa363d8d
RuslanZb <zubairov.rr@mail.ru>
2023-02-22 14:54:00 +0300

    Рефакторинг под Checkstyle 3.1

--- src/main/java/ru/job4j/io/Buffered.java
+++ src/main/java/ru/job4j/io/Buffered.java
@@ -8,7 +8,7 @@ import java.io.FileOutputStream;
 public class Buffered {
     public static void main(String[] args) {
         try (BufferedInputStream in = new BufferedInputStream(new FileInputStream("data/input.txt"));
-             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("data/output.txt",true))) {
+             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("data/output.txt", true))) {
             out.write(in.readAllBytes());
         } catch (Exception e) {
             e.printStackTrace();
