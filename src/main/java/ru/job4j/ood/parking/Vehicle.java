509081cb39b36c32e07165cd6291787474ebbf0b
RuslanZb <zubairov.rr@mail.ru>
2023-03-27 12:56:09 +0300

    2. Парковка машин [#853 #404543]

--- src/main/java/ru/job4j/ood/parking/Vehicle.java
+++ src/main/java/ru/job4j/ood/parking/Vehicle.java
@@ -29,8 +29,12 @@ public abstract class Vehicle {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Vehicle vehicle = (Vehicle) o;
         return Objects.equals(number, vehicle.number);
     }
